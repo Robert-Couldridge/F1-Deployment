6ad2ae2660666f1edf80d431e411810d362d2990
Robert Couldridge <robert.couldridge@bt.com>
2025-03-20 15:00:15 +0000

    updated readme and .gitignore and formatted terraform files

---
+++ README.md
@@ -0,0 +1,14 @@
+# F1 Overtake Prediction Deployment
+
+## Usage
+This Terraform codebase creates an AWS application to predict the number of laps 
+till one car overtakes another
+
+#### Useful Terraform Commands
+- `terraform init` , intialises terraform, run this when you first clone the 
+repo or if you change modules or backend configuration
+- `terraform fmt` , auto formats your terraform files
+- `terraform validate` , reviews your terraform code and points out errors
+- `terraform plan` , displays what terraform is about to provision 
+- `terraform apply` , displays the plan and provides the option to provision that plan
+- `terraform destroy` , destroys the infrastructure created by terraform
--- variables.tf
+++ variables.tf
@@ -1,22 +1,22 @@
 variable "application_region" {
   description = "The Chosen AWS region for the application"
-  type = string
-  default = "us-east-1"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "acountId" {
   description = "The AWS account ID"
-  type = string
+  type        = string
 }
 
 variable "lambda_function_name" {
   description = "The name of the lambda function"
-  type = string
-  default = "Overtake prediction"
+  type        = string
+  default     = "Overtake prediction"
 }
 
 variable "endpoint_path" {
   description = "The GET endpoint path"
-  type = string
-  default = "overtake_prediction"
+  type        = string
+  default     = "overtake_prediction"
 }
