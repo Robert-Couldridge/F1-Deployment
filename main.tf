2a70d174b21ea9004ccb8fe61191d1002c45ea29
Robert Couldridge <robert.couldridge@bt.com>
2025-03-20 18:36:43 +0000

    continued adjustments to labrole requirements

--- main.tf
+++ main.tf
@@ -32,6 +32,67 @@ resource "aws_lambda_function" "application_lambda_func" {
   depends_on = [aws_cloudwatch_log_group.application_cloudwatch_log_group]
 }
 
+# IAM role creation (may not work in lab)
+
+# resource "aws_iam_role" "lambda_role" {
+#   name               = "terraform_aws_lambda_role"
+#   assume_role_policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Action": "sts:AssumeRole",
+#       "Principal": {
+#         "Service": "lambda.amazonaws.com"
+#       },
+#       "Effect": "Allow",
+#       "Sid": ""
+#     }
+#   ]
+# }
+# EOF
+# }
+
+# IAM policy for logging from a lambda
+
+# resource "aws_iam_policy" "iam_policy_for_lambda" {
+
+#   name        = "aws_iam_policy_for_terraform_aws_lambda_role"
+#   path        = "/"
+#   description = "AWS IAM Policy for managing aws lambda role"
+#   policy      = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Action": [
+#         "logs:CreateLogGroup",
+#         "logs:CreateLogStream",
+#         "logs:PutLogEvents"
+#       ],
+#       "Resource": "arn:aws:logs:*:*:*",
+#       "Effect": "Allow"
+#     },
+#     {
+#       "Action": [
+#       "lambda:InvokeFunction"
+#       ],
+#       "Resource": "*",
+#       "Effect": "Allow"
+#     }
+#   ]
+# }
+# EOF
+# }
+
+# # Policy Attachment on the role.
+
+# resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_lab_role" {
+#   role       = var.lab_role
+#   policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
+# }
+
+
 # Create cloudwatch log group
 
 resource "aws_cloudwatch_log_group" "application_cloudwatch_log_group" {
